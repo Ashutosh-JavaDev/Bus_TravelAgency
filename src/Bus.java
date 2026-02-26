d68c3a9df5d80ff0610dc0fe7d845e5e766b3c36
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-26 10:27:57 +0530

    Connection Established

---
+++ src/Bus.java
@@ -0,0 +1,30 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+public class Bus {
+    private static final String url = "jdbc:mysql://localhost:3306/Bus_Travel";
+    private static final String username = "root";
+    private static final String password = "@Radhakrishna297";
+
+    Bus() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection=DriverManager.getConnection(url, username, password);
+            Statement statement=connection.createStatement();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Class Not Found", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Bus();
+    }
+}
